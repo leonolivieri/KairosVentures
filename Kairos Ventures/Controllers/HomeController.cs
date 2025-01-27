c7cb096aad334ff6e0a3df94161cbf0b4267fdfa
Leonel Feliciano de Oliveira <Leon2010.sp@hotmail.com>
2025-01-27 16:08:11 -0300

    primeiro commit

---
+++ Kairos Ventures/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Kairos_Ventures.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Kairos_Ventures.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Cliente_Consulta()
+
+        { return View(); }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Kairos Ventures/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kairos_Ventures.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
